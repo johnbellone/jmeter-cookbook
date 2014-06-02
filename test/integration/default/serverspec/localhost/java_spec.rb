423ae49302bc28494bef83c8b46fe3a316058e68
John Bellone <jbellone@bloomberg.net>
2014-06-02 12:06:52 -0400

    Fix those tests and make them green!

--- test/integration/default/serverspec/localhost/java_spec.rb
+++ test/integration/default/serverspec/localhost/java_spec.rb
@@ -1,5 +1,5 @@
 require 'spec_helper'
 
-describe package('java') do
+describe package('java-1.6.0-openjdk') do
   it { should be_installed }
 end
--- test/integration/default/serverspec/localhost/jmeter_spec.rb
+++ test/integration/default/serverspec/localhost/jmeter_spec.rb
@@ -3,11 +3,11 @@ require 'spec_helper'
 describe file('/usr/local/bin/jmeter') do
   it { should be_file }
   it { should be_executable }
-  it { should be_symlinked_to('/usr/local/jmeter-2.11/bin/jmeter') }
+  it { should be_linked_to('/usr/local/jmeter-2.11/bin/jmeter') }
 end
 
 describe file('/usr/local/bin/jmeter-server') do
   it { should be_file }
   it { should be_executable }
-  it { should be_symlinked_to('/usr/local/jmeter-2.11/bin/jmeter-server') }
+  it { should be_linked_to('/usr/local/jmeter-2.11/bin/jmeter-server') }
 end
