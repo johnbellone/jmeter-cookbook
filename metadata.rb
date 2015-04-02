d554a32cff7333055385b943d9eb0e06cff75be4
John Bellone <jbellone@bloomberg.net>
2015-04-02 08:45:09 -0400

    Bump version of cookbook to 1.0.0.

--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,10 @@
-name             'jmeter'
-maintainer       'John Bellone'
+name 'jmeter'
+maintainer 'John Bellone'
 maintainer_email 'jbellone@bloomberg.net'
-license          'Apache 2.0'
-description      'Installs/Configures jmeter for load-testing.'
+license 'Apache 2.0'
+description 'Installs/Configures jmeter for load-testing.'
 long_description 'Installs/Configures jmeter for load-testing.'
-version          '0.2.3'
+version '1.0.0'
 
 %w(centos redhat).each do |name|
   supports name, '~> 7.0'
@@ -14,9 +14,5 @@ end
 supports 'ubuntu', '= 12.04'
 supports 'ubuntu', '= 14.04'
 
-depends 'apt'
 depends 'ark'
-depends 'build-essential'
-depends 'chef-sugar'
 depends 'java'
-depends 'poise'
